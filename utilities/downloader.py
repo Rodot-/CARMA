152f768c6661291cc051dd4d2c186037e27c3b53
rodot- <jto33@drexel.edu>
2017-10-21 06:39:15 -0400

    Made the style a little better, still need to improve to comply with PEP 8

--- utilities/downloader.py
+++ utilities/downloader.py
@@ -9,10 +9,10 @@ from astropy.io import fits
 
 def downloadVJ(EPIC, campaign):
 	EPIC = str(EPIC)
-	base_url = "http://archive.stsci.edu/missions/hlsp/k2sff/"
-	directory = "c%02i/%s00000/%s/hlsp_k2sff_k2_lightcurve_%s-c%02i_kepler_v1_llc.fits"
-	directory = directory % (campaign, EPIC[:4], EPIC[4:], EPIC, campaign)
-	filename = base_url + directory
+	base_url = "http://archive.stsci.edu/missions/hlsp/k2sff"
+	path = "c%02i/%s00000/%s" % (campaign, EPIC[:4], EPIC[4:])
+	base_name = "hlsp_k2sff_k2_lightcurve_%s-c%02i_kepler_v1_llc.fits" % (EPIC, campaign)
+	filename = '/'.join((base_url, path, base_name))
 	result = None
 	for i in xrange(5): #Timeouts are common, just need to retry
 		try:
