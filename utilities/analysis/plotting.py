b40f7c50b8fd119fbab89999db7fe5caa29afe01
rodot- <jto33@drexel.edu>
2018-07-11 17:52:09 -0400

    Added functions for plotting pixel lc image maps and pixel lcs themselves

--- utilities/analysis/plotting.py
+++ utilities/analysis/plotting.py
@@ -32,8 +32,55 @@ def plot_nice_intervals(ax, x, y, N=30, label=None, color=None, scatter=False):
 		ax.plot(x, y, ls=' ', marker='.', ms=1, color=color)
 		ax.fill_between(x, rm+rs, rm-rs, color=color, alpha=0.1, linestyle='None')
 
-	ax.plot(x, rm, ls='-', color=color, lw=2, label=label)		
+	return ax.plot(x, rm, ls='-', color=color, lw=2, label=label)		
 
 
+def plot_pixel_lc(ax, lc, smooth=False, image=False, differenced=False, **kwargs):
+	'''Plot a pixel lc as either a set of light curves or an image
+	in which rows represent light curves
+
+	args:
+	ax - matplotlib Axes object to plot to
+	lc - NxM numpy array of light curves
+	smooth - Bool or int of boxcar smoothing window over which to smooth the light curve
+	image - Bool, whether to plot individual light curves or make an image plot
+	differenced - Bool, whether to difference the rows of the light curve
+	cmap - matplotlib color map to apply to the image
+	'''
+
+	# create the image from the rows of the light curve
+	image_ = lc if not differenced else lc[:, 1:] - lc[:, :-1]
+	
+	# whiten the light curve
+	image_ -= np.nanmedian(image_, axis=0)
+	image_ /= np.nanstd(image_, axis=0)
+
+	# optionally smooth the light curve
+	if smooth:
+		N = 30 if smooth is True else smooth
+		image_[::] = running_median(image_, N, axis=0)
+	
+	if image:
+		return ax.imshow(image_.T[::-1], **kwargs)
+	
+	return [ax.plot(line, **kwargs) for line in image_.T] 
+
+
+def format_pixel_image(ax, im, n_epochs, cadence=0.2043229):
+	'''apply some additional formatting to the pixel image plot
+	These basically apply my own formattings like I prefer'''
+	
+	ax.set_yticks(np.linspace(0, ax.get_ybound()[1]+1, 10))
+	ax.set_xticks(np.linspace(0, ax.get_xbound()[1]+1, 10))
+
+	ax.set_yticklabels(np.linspace(100,0,11).astype(int))
+	ax.set_xticklabels(map('{:.0f}'.format,np.arange(11)*cadence*n_epochs/11))
+
+	ax.set_xlabel('Time (days)', fontsize=24)
+	ax.set_ylabel('Percentile', fontsize=24)
+
+	cbar = ax.figure.colorbar(im, ticks=np.linspace(-1,1,3), pad=0.1)
+
+	ax.tick_params(axis='both', which='both', labelsize=24) 
 
 
