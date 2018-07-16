faa5408e14c4de77222b8362710c1d44e84c3e91
rodot- <jto33@drexel.edu>
2018-07-16 19:03:59 -0400

    Added hst to the __init__.py imports

--- utilities/queries/__init__.py
+++ utilities/queries/__init__.py
@@ -1,4 +1,5 @@
 '''a small library of querying tools'''
 import mast as mast # base querying tools
 import k2 as k2 #specific to k2
+import hst as hst
 import search as search # get objects in a k2 campaign
